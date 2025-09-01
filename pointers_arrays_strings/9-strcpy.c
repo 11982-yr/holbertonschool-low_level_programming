3685435f3cc185eb16e158cf56c458e6f0a688d7
root <11982@holbertonstudents.com>
2025-09-01 16:37:05 +0000

    Pointers, arrays and strings

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string pointed to by src.
+ * @dest: the desination of the string.
+ * @src: the source of the string.
+ *
+ * Return: a pointer to the destination string @dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+			index++;
+	}
+	return (dest);
+}

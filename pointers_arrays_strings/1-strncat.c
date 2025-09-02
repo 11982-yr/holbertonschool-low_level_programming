3319ad046aedd579b988b642a0f3b28f774d5c14
root <11982@holbertonstudents.com>
2025-09-02 14:37:21 +0000

    Pointers, arrays and strings

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings.
+ * @dest: the destination of the string.
+ * @src: the source of the string.
+ * @n: the number of bytes.
+ *
+ * Return: a pointer to the resulting string @dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}

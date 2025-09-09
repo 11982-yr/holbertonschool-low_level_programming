07ddc244e3e92efbc9213631f0f1ad1a5568bc19
root <11982@holbertonstudents.com>
2025-09-09 15:18:08 +0000

    Argv, Argc

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * @argc: argument count.
+ * @argv: argument values.
+ *
+ * Return: If the program recieves two arguments - 0.
+ * If the program does not recieve two arguments - 1.
+ */
+
+int main(int argc, char *argv[])
+{
+	

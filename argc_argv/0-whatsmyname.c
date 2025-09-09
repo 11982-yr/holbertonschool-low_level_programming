d57292ea82ff6a1769a6999df01cebd49520fb89
root <11982@holbertonstudents.com>
2025-09-09 14:08:58 +0000

    Argv, Argc

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * @argc: argument count.
+ * @argv: argument values.
+ *
+ * Return: Always 0.
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

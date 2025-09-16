2c409797e61cb6cad6bdaecab2d91952dfe32414
root <11982@holbertonstudents.com>
2025-09-16 16:40:18 +0000

    Structures, typedef

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog.
+ *
+ * @name: the name of the dog.
+ * @age: the age of the dog.
+ * @owner: the owner of the dog.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+

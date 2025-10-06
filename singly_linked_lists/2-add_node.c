253cb90e90c534ace8eeb6c082058b804aec6568
root <11982@holbertonstudents.com>
2025-10-06 15:36:21 +0000

    Singly Linked Lists

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <string.h>
 
 /**
  * add_node - adds a new node at the beginning of a list_t list
@@ -19,11 +20,21 @@ list_t *add_node(list_t **head, const char *str)
 	if (new == NULL)
 		return (NULL);
 
-	dup = strdup(str)
+	dup = strdup(str);
 		if (dup == NULL)
 		{
-			free (new);
-			return (dup);
+			free(new);
+			return (NULL);
 		}
 
-	for (len = 0; str[len])
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = dup;
+	new->len = len;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}

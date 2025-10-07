c35b76d15b6933cbcdb6a16fe7c228c5f9c15d73
root <11982@holbertonstudents.com>
2025-10-07 14:40:48 +0000

    Singly Linked Lists

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * free_list: frees a list_t list
+ *
+ * @head: the head of the list.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}

d0f4761afdaeaba043fc64cd53c1d87891a8a660
root <11982@holbertonstudents.com>
2025-10-07 15:07:32 +0000

    Doubly Linked Lists

---
+++ doubly_linked_lists/lists.h
@@ -0,0 +1,8 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+size_t print_dlistint(const dlistint_h *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+

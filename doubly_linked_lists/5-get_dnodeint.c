a8f8c8fbb07abc129da859665e191575554042ae
root <11982@holbertonstudents.com>
2025-10-08 14:21:20 +0000

    Doubly Linked Lists

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -10,4 +10,11 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-
+	for (; index != 0; index--)
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+	}
+	return (head);
+}

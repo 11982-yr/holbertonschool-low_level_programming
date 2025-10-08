020d7e39d684f85d39145bd618fc9982ed052215
root <11982@holbertonstudents.com>
2025-10-08 13:58:27 +0000

    Doubly Linked Lists

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,7 +9,7 @@
  * Return: the address of the new element, NULL if failed.
  */
 
-dlistint_t *addnodeint_end(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new, *last;
 
@@ -17,7 +17,21 @@ dlistint_t *addnodeint_end(dlistint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 
-	n->new = n;
-	n->prev = NULL;
+	new->n = n;
+	new->next = NULL;
 
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
 
+	last = *head;
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new;
+	new->prev = last;
+
+	return (new);
+}

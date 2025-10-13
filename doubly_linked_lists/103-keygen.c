98195ab25a424d727d9a67427c76ad3cfad85ff0
root <11982@holbertonstudents.com>
2025-10-13 15:35:34 +0000

    Doubly Linked Lists

---
+++ doubly_linked_lists/103-keygen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - a keygen for crackme
+ *
+ * @argc: argument count
+ * @argv: argument value
+ *
+ * Return: Always 0.
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	char password[7], *codex;
+	int len = strlen(argv[1]), i, tmp;
+
+	*codex = "A-CHRDw87INS0E9B2TibgpnMVys5Xzvt";
+
+
